de483c39ae96ebdfa5e961b5c57033d681e100d8
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2022-07-31 20:56:47 -0400

    edited javascript

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = "0123456789"; 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"; 
+var numbers = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var symbols = "!@#$%^&*()"; 
+var symbols = "!@#$%^&*()";
 var password = "";
 
 
-
 function generatePassword() {
-var passwordLength = prompt("How long do you want your password?")
+  var passwordLength = prompt("How long do you want your password?")
   console.log(passwordLength)
-  if (passwordLength < 8 || passwordLength >128 ) {
-    alert ("Must be at least 8 characters long, no more than 128 characters")
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Must be at least 8 characters long, no more than 128 characters")
   }
-var useNumbers = confirm("Do you want to use numbers?")
-console.log(numbers)
-if (useNumbers) {
-}
- var useLowerCase = confirm("Do you want to use lowercase letters?")
- console.log(lowerCase)
- 
- if (useLowerCase){
-}
-var useUpperCase = confirm("Do you want to use uppercase letters?")
-console.log(upperCase)
-if (useUpperCase){
-}
-var useSymbols = confirm("Do you want to use symbols?")
-console.log(symbols)
-if (useSymbols){
-}
+  var useNumbers = confirm("Do you want to use numbers?")
+  var useLowerCase = confirm("Do you want to use lowercase letters?")
+  var useUpperCase = confirm("Do you want to use uppercase letters?")
+  var useSymbols = confirm("Do you want to use symbols?")
+  console.log(numbers)
+  for(var i = 0; i < passwordLength; i++){
+    if (useNumbers && password.length< passwordLength) {
+      var positionNumber = Math.floor(Math.random() * numbers.length)
+      password = password + numbers[positionNumber]
+    }
+  
+    
+    console.log(lowerCase)
+  
+    if (useLowerCase && password.length< passwordLength) {
+    var positionNumber = Math.floor(Math.random() * lowerCase.length)
+     password = password + lowerCase[positionNumber]
+    }
+  
+    console.log(upperCase)
+    if (useUpperCase && password.length< passwordLength) {
+      var positionNumber = Math.floor(Math.random() * upperCase.length)
+      password = password + upperCase[positionNumber]
+    }
+   
+    console.log(symbols)
+    if (useSymbols && password.length< passwordLength) {
+      var positionNumber = Math.floor(Math.random() * symbols.length)
+      password = password + symbols[positionNumber]
+    }
+  }
+  if (useNumbers === false && useLowerCase === false && useUpperCase === false && useSymbols === false){
+    alert("You must have one type of variable.")
+  }
+  return password
 }
 
 // Write password to the #password input
@@ -38,22 +55,7 @@ function writePassword() {
   console.log("function started")
   var password = generatePassword();
   var passwordText = document.querySelector("#password")
-function randomLowCase() {
-  return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-}
-console.log(randomLowCase());
-function randomUppCase () {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-console.log(randomUppCase());
-function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
- } console.log(randomNumber()){
- }
- function randomSymbol() {
-  const symbols = "!@#$%^&*()";
-  return symbols [Math.floor(Math.random()) *symbols.length];
- }console.log(randomSymbol());
+  passwordText.textContent = password
 }
 
 // Add event listener to generate button
